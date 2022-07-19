f35578213df8fd4eb1af2c1264da9fa6b9645f09
bwingo47 <wingobruce47@gmail.com>
2022-07-18 22:06:57 -0400

    clean cmake-build-debug

--- ocs2_robotic_examples/ocs2_legged_robot_ros/cmake-build-debug/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /root/docker_ws/src/ocs2/ocs2_robotic_examples/ocs2_legged_robot_ros/cmake-build-debug/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
